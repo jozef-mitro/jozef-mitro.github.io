c09d976b0aef37600b64bd8c32e142a3da9f0bd1
Jozef Mitro <jozef.mitro.sk@gmail.com>
2024-03-28 11:54:34 +0100

    Add min total score and allowed sources

---
+++ js/ose_classes.js
@@ -0,0 +1,432 @@
+let OseClasses = [
+    // Classic Fantasy
+    {
+        name: "Cleric",
+        requirements: [],
+        primeRequisite: "wis",
+        source: "Classic Fantasy"
+    },
+    {
+        name: "Dwarf",
+        requirements: ["con"],
+        primeRequisite: "str",
+        source: "Classic Fantasy"
+    },
+    {
+        name: "Elf",
+        requirements: ["int"],
+        primeRequisite: function (scores) {
+            if (scores.int >= 16 && scores.str >= 13) {
+                return 10;
+            } else if (scores.int >= 13 && scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Classic Fantasy"
+    },
+    {
+        name: "Fighter",
+        requirements: [],
+        primeRequisite: "str",
+        source: "Classic Fantasy"
+    },
+    {
+        name: "Halfling",
+        requirements: ["con", "dex"],
+        primeRequisite: function(scores) {
+            if (scores.dex >= 13 && scores.str >= 13) {
+                return 10;
+            } else if (scores.dex >= 13 || scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Classic Fantasy"
+    },
+    {
+        name: "Magic-User",
+        requirements: [],
+        primeRequisite: "int",
+        source: "Classic Fantasy"
+    },
+    {
+        name: "Thief",
+        requirements: [],
+        primeRequisite: "dex",
+        source: "Classic Fantasy"
+    },
+    // Advanced Fantasy
+    {
+        name: "Acrobat",
+        requirements: [],
+        primeRequisite: "dex",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Assassin",
+        requirements: [],
+        primeRequisite: "dex",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Barbarian",
+        requirements: ["dex"],
+        primeRequisite: function(scores) {
+            if (scores.con >= 16 && scores.str >= 16) {
+                return 10;
+            } else if (scores.con >= 13 || scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Bard",
+        requirements: ["dex", "int"],
+        primeRequisite: "cha",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Drow",
+        requirements: ["int"],
+        primeRequisite: function(scores) {
+            if (scores.wis >= 16 && scores.str >= 13) {
+                return 10;
+            } else if (scores.wis >= 13 && scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Druid",
+        requirements: [],
+        primeRequisite: "wis",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Duergar",
+        requirements: ["con", "int"],
+        primeRequisite: "str",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Gnome",
+        requirements: ["con"],
+        primeRequisite: function(scores) {
+            if (scores.int >= 16 && scores.dex >= 13) {
+                return 10;
+            } else if (scores.int >= 13 && scores.dex >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Half-Elf",
+        requirements: ["cha", "con"],
+        primeRequisite: function(scores) {
+            if ((scores.int >= 16 && scores.str >= 13) || (scores.str >= 16 && scores.int >= 13)) {
+                return 10;
+            } else if (scores.int >= 13 && scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Half-Orc",
+        requirements: [],
+        primeRequisite: function(scores) {
+            if (scores.str >= 16 && scores.dex >= 16) {
+                return 10;
+            } else if (scores.str >= 13 && scores.dex >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Illusionist",
+        requirements: ["dex"],
+        primeRequisite: "int",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Knight",
+        requirements: ["con", "dex"],
+        primeRequisite: "str",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Paladin",
+        requirements: ["cha"],
+        primeRequisite: function(scores) {
+            if (scores.str >= 16 && scores.wis >= 16) {
+                return 10;
+            } else if (scores.str >= 13 || scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Ranger",
+        requirements: ["con", "wis"],
+        primeRequisite: "str",
+        source: "Advanced Fantasy"
+    },
+    {
+        name: "Svirfneblin",
+        requirements: ["con"],
+        primeRequisite: "str",
+        source: "Advanced Fantasy"
+    },
+    // Carcass Crawler
+    {
+        name: "Arcane Bard",
+        requirements: ["int"],
+        primeRequisite: function(scores) {
+            if ((scores.cha >= 16 && scores.dex >= 13) || (scores.dex >= 16 && scores.cha >= 13)) {
+                return 10;
+            } else if (scores.cha >= 13 && scores.dex >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 0"
+    },
+    {
+        name: "Beast Master",
+        requirements: [],
+        primeRequisite: function(scores) {
+            if (scores.str >= 16 && scores.wis >= 16) {
+                return 10;
+            } else if (scores.str >= 13 || scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 0 and 3"
+    },
+    {
+        name: "Changeling",
+        requirements: ["int"],
+        primeRequisite: function(scores) {
+            if (scores.cha >= 16 && scores.dex >= 16) {
+                return 10;
+            } else if (scores.cha >= 13 || scores.dex >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 0"
+    },
+    {
+        name: "Chaos Knight",
+        requirements: ["cha"],
+        primeRequisite: function(scores) {
+            if (scores.str >= 16 && scores.wis >= 16) {
+                return 10;
+            } else if (scores.str >= 13 || scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 0"
+    },
+    {
+        name: "Mage",
+        requirements: [],
+        primeRequisite: function(scores) {
+            if (scores.int >= 16 && scores.wis >= 13) {
+                return 10;
+            } else if (scores.int >= 13 && scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 0 and 1"
+    },
+    {
+        name: "Mutoid",
+        requirements: [],
+        primeRequisite: "dex",
+        source: "Carcass Crawler 0 and 3"
+    },
+    {
+        name: "Mycelian",
+        requirements: ["con"],
+        primeRequisite: "str",
+        source: "Carcass Crawler 0 and 3"
+    },
+    {
+        name: "Warden",
+        requirements: ["con", "wis"],
+        primeRequisite: "str",
+        source: "Carcass Crawler 0"
+    },
+    {
+        name: "Acolyte",
+        requirements: [],
+        primeRequisite: "wis",
+        source: "Carcass Crawler 1"
+    },
+    {
+        name: "Gargantua",
+        requirements: ["con", "str"],
+        primeRequisite: function(scores) {
+            if (scores.str >= 16 && scores.con >= 13) {
+                return 10;
+            } else if (scores.con >= 13 && scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 1"
+    },
+    {
+        name: "Goblin",
+        requirements: ["dex"],
+        primeRequisite: function(scores) {
+            if (scores.dex >= 16 && scores.str >= 16) {
+                return 10;
+            } else if (scores.dex >= 13 || scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 1"
+    },
+    {
+        name: "Hephaestan",
+        requirements: ["cha", "con"],
+        primeRequisite: function(scores) {
+            if (scores.int >= 16 && scores.wis >= 13) {
+                return 10;
+            } else if (scores.int >= 13 || scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 1"
+    },
+    {
+        name: "Kineticist",
+        requirements: [],
+        primeFive: "dex 13 and wis 13",
+        primeTen: "dex 16 and wis 16",
+        primeRequisite: function(scores) {
+            if (scores.dex >= 16 && scores.wis >= 16) {
+                return 10;
+            } else if (scores.dex >= 13 || scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 1"
+    },
+    {
+        name: "Phase Elf",
+        requirements: ["int"],
+        primeRequisite: function(scores) {
+            if (scores.int >= 16 && scores.str >= 13) {
+                return 10;
+            } else if (scores.int >= 13 && scores.str >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 2"
+    },
+    {
+        name: "Wood Elf",
+        requirements: ["dex", "int"],
+        primeRequisite: function(scores) {
+            if (scores.dex >= 16 && scores.wis >= 13) {
+                return 10;
+            } else if (scores.dex >= 13 && scores.wis >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 2"
+    },
+    {
+        name: "Dragonborn",
+        requirements: ["con", "int"],
+        primeRequisite: "str",
+        source: "Carcass Crawler 2"
+    },
+    {
+        name: "Tiefling",
+        requirements: ["int"],
+        primeRequisite: function(scores) {
+            if (scores.cha >= 16 && scores.dex >= 16) {
+                return 10;
+            } else if (scores.cha >= 13 || scores.dex >= 13) {
+                return 5;
+            } else {
+                return 0;
+            }
+        },
+        source: "Carcass Crawler 2"
+    },
+    // Other
+    {
+        name: "Necromancer",
+        requirements: ["wis"],
+        primeRequisite: "int",
+        source: "The Necromancer"
+    }
+]
+
+function getOseClasses(allowedSources) {
+    return OseClasses.filter(c => allowedSources.some(source => c.source.startsWith(source)));
+}
+
+function getExpBonus(classData, scores) {
+    if (typeof classData.primeRequisite === "string") {
+        let primeRequisite = classData.primeRequisite;
+
+        if (scores[primeRequisite] >= 16) {
+            return 10;
+        } else if (scores[primeRequisite] >= 13) {
+            return 5;
+        } else if (scores[primeRequisite] >= 9) {
+            return 0;
+        } else if (scores[primeRequisite] >= 6) {
+            return -10;
+        } else {
+            return -20;
+        }
+    } else if (typeof classData.primeRequisite === "function") {
+        return classData.primeRequisite(scores);
+    }
+}
--- js/rpg.js
+++ js/rpg.js
@@ -2,8 +2,25 @@ function generateCharacters(event) {
     event.preventDefault();
 
     let numCharacters = document.getElementById("numCharacters").value;
+    let minScoreTotal = document.getElementById("minScoreTotal").value;
+    let allowedSources = [];
 
-    // For each character pick a random gender then pick a random name.
+    if (document.getElementById("sourceClassicFantasy").checked) {
+        allowedSources.push("Classic Fantasy");
+    }
+
+    if (document.getElementById("sourceAdvancedFantasy").checked) {
+        allowedSources.push("Advanced Fantasy");
+    }
+
+    if (document.getElementById("sourceCarcassCrawler").checked) {
+        allowedSources.push("Carcass Crawler");
+    }
+
+    if (document.getElementById("sourceTheNecromancer").checked) {
+        allowedSources.push("The Necromancer");
+    }
+
     let characters = [];
 
     for (let i = 0; i < numCharacters; i++) {
@@ -11,30 +28,32 @@ function generateCharacters(event) {
         character.gender = Math.random() < 0.5 ? "Masculine" : "Feminine";
         character.pronouns = character.gender === "Masculine" ? "He/Him" : "She/Her";
         character.name = getSingleName(character.gender);
+        let scoreTotal = 0;
 
-        character.str = roll3d6();
-        character.int = roll3d6();
-        character.wis = roll3d6();
-        character.dex = roll3d6();
-        character.con = roll3d6();
-        character.cha = roll3d6();
+        while (scoreTotal < minScoreTotal) {
+            character.scores = {
+                str: roll3d6(),
+                int: roll3d6(),
+                wis: roll3d6(),
+                dex: roll3d6(),
+                con: roll3d6(),
+                cha: roll3d6()
+            }
 
-        character.strMod = getModifier(character.str);
-        character.intMod = getModifier(character.int);
-        character.wisMod = getModifier(character.wis);
-        character.dexMod = getModifier(character.dex);
-        character.conMod = getModifier(character.con);
-        character.chaMod = getModifier(character.cha);
-
-        let scores = {
-            str: character.str,
-            int: character.int,
-            wis: character.wis,
-            dex: character.dex,
-            con: character.con,
-            cha: character.cha
+            scoreTotal = Object.values(character.scores).reduce((total, score) => total + score);
         }
-        character.class = getCharacterClass(scores);
+
+        character.modifiers = {
+            str: getModifier(character.scores.str),
+            int: getModifier(character.scores.int),
+            wis: getModifier(character.scores.wis),
+            dex: getModifier(character.scores.dex),
+            con: getModifier(character.scores.con),
+            cha: getModifier(character.scores.cha)
+        }
+
+        character.class = getCharacterClass(character.scores, allowedSources);
+        character.expBonus = character.class.expBonus;
 
         characters.push(character);
     }
@@ -43,9 +62,11 @@ function generateCharacters(event) {
     charactersElement.innerHTML = "";
     characters.forEach(character => {
         charactersElement.innerHTML += `${character.name} [${character.pronouns}]<br>`;
-        charactersElement.innerHTML += `Str: ${character.str} (${character.strMod}), Int: ${character.int} (${character.intMod}), Wis: ${character.wis} (${character.wisMod}), Dex: ${character.dex} (${character.dexMod}), Con: ${character.con} (${character.conMod}), Cha: ${character.cha} (${character.chaMod})<br>`;
+        charactersElement.innerHTML += `Scores: STR ${character.scores.str} (${character.modifiers.str}), INT ${character.scores.int} (${character.modifiers.int}), WIS ${character.scores.wis} (${character.modifiers.wis}), DEX ${character.scores.dex} (${character.modifiers.dex}), CON ${character.scores.con} (${character.modifiers.con}), CHA ${character.scores.cha} (${character.modifiers.cha})<br>`
+        // Scores as a tab separated list.
+        charactersElement.innerHTML += `${Object.values(character.scores).join("\t")}<br>`;
         charactersElement.innerHTML += `Class: ${character.class.name} (${character.class.source})<br>`;
-        charactersElement.innerHTML += `Experience Bonus: ${character.class.expBonus}<br><br>`;
+        charactersElement.innerHTML += `Experience Bonus: ${character.expBonus}%<br><br>`;
     });
 }
 
@@ -79,65 +100,25 @@ function getModifier(score) {
     }
 }
 
-// Remove the unused variable 'availableClasses'
-// and fill parameters primeFive for each character class.
-function getCharacterClass(scores) {
-    let availableClasses = [
-        { name: "Cleric", requiredScores: [], primeFive: "wis", primeTen: "", source: "Classic Fantasy" },
-        { name: "Dwarf", requiredScores: ["con"], primeFive: "str", primeTen: "", source: "Classic Fantasy" },
-        { name: "Elf", requiredScores: ["int"], primeFive: "int 13 and str 13", primeTen: "int 16 and str 13", source: "Classic Fantasy" },
-        { name: "Fighter", requiredScores: [], primeFive: "str", primeTen: "", source: "Classic Fantasy" },
-        { name: "Halfling", requiredScores: ["con", "dex"], primeFive: "dex 13 or str 13", primeTen: "dex 13 and str 13", source: "Classic Fantasy" },
-        { name: "Magic-User", requiredScores: [], primeFive: "int", primeTen: "", source: "Classic Fantasy" },
-        { name: "Thief", requiredScores: [], primeFive: "dex", primeTen: "", source: "Classic Fantasy" },
-        { name: "Acrobat", requiredScores: [], primeFive: "dex", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Assassin", requiredScores: [], primeFive: "dex", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Barbarian", requiredScores: ["dex"], primeFive: "con 13 or str 13", primeTen: "con 16 and str 16", source: "Advanced Fantasy" },
-        { name: "Bard", requiredScores: ["dex", "int"], primeFive: "cha", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Drow", requiredScores: ["int"], primeFive: "str 13 and wis 13", primeTen: "str 13 and wis 16", source: "Advanced Fantasy" },
-        { name: "Druid", requiredScores: [], primeFive: "wis", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Duergar", requiredScores: ["con", "int"], primeFive: "str", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Gnome", requiredScores: ["con"], primeFive: "dex 13 and int 13", primeTen: "dex 13 and int 16", source: "Advanced Fantasy" },
-        { name: "Half-Elf", requiredScores: ["cha", "con"], primeFive: "int 13 and str 13", primeTen: "int or str 16 and the other 13", source: "Advanced Fantasy" },
-        { name: "Half-Orc", requiredScores: [], primeFive: "dex 13 and str 13", primeTen: "dex 16 and str 16", source: "Advanced Fantasy" },
-        { name: "Illusionist", requiredScores: ["dex"], primeFive: "int", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Knight", requiredScores: ["con", "dex"], primeFive: "str", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Paladin", requiredScores: ["cha"], primeFive: "str 13 or wis 13", primeTen: "str 16 and wis 16", source: "Advanced Fantasy" },
-        { name: "Ranger", requiredScores: ["con", "wis"], primeFive: "str", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Svirfneblin", requiredScores: ["con"], primeFive: "str", primeTen: "", source: "Advanced Fantasy" },
-        { name: "Arcane Bard", requiredScores: ["int"], primeFive: "cha 13 and dex 13", primeTen: "cha or dex 16 and the other 13", source: "Carcass Crawler 0" },
-        { name: "Beast Master", requiredScores: [], primeFive: "str 13 or wis 13", primeTen: "str 16 and wis 16", source: "Carcass Crawler 0 and 3" },
-        { name: "Changeling", requiredScores: ["int"], primeFive: "cha 13 or dex 13", primeTen: "cha 16 and dex 16", source: "Carcass Crawler 0" },
-        { name: "Chaos Knight", requiredScores: ["cha"], primeFive: "str 13 or wis 13", primeTen: "str 16 and wis 16", source: "Carcass Crawler 0" },
-        { name: "Mage", requiredScores: [], primeFive: "int 13 and wis 13", primeTen: "int 16 and wis 13", source: "Carcass Crawler 0 and 1" },
-        { name: "Mutoid", requiredScores: [], primeFive: "dex", primeTen: "", source: "Carcass Crawler 0 and 3" },
-        { name: "Mycelian", requiredScores: ["con"], primeFive: "str", primeTen: "", source: "Carcass Crawler 0 and 3" },
-        { name: "Warden", requiredScores: ["con", "wis"], primeFive: "str", primeTen: "", source: "Carcass Crawler 0" },
-        { name: "Acolyte", requiredScores: [], primeFive: "wis", primeTen: "", source: "Carcass Crawler 1" },
-        { name: "Gargantua", requiredScores: ["con", "str"], primeFive: "con 13 and str 13", primeTen: "str 16 and con 13", source: "Carcass Crawler 1" },
-        { name: "Goblin", requiredScores: ["dex"], primeFive: "dex 13 or str 13", primeTen: "dex 16 and str 16", source: "Carcass Crawler 1" },
-        { name: "Hephaestan", requiredScores: ["cha", "con"], primeFive: "int 13 and wis 13", primeTen: "int 16 and wis 13", source: "Carcass Crawler 1" },
-        { name: "Kineticist", requiredScores: [], primeFive: "dex 13 and wis 13", primeTen: "dex 16 and wis 16", source: "Carcass Crawler 1" },
-        { name: "Phase Elf", requiredScores: ["int"], primeFive: "int 13 and str 13", primeTen: "int 16 and str 13", source: "Carcass Crawler 2" },
-        { name: "Wood Elf", requiredScores: ["dex", "int"], primeFive: "dex 13 and wis 13", primeTen: "dex 16 and wis 13", source: "Carcass Crawler 2" },
-        { name: "Dragonborn", requiredScores: ["con", "int"], primeFive: "str", primeTen: "", source: "Carcass Crawler 2" },
-        { name: "Tiefling", requiredScores: ["int"], primeFive: "cha 13 or dex 13", primeTen: "cha 16 and dex 16", source: "Carcass Crawler 2" },
-        { name: "Necromancer", requiredScores: ["wis"], primeFive: "int", primeTen: "", source: "The Necromancer" }
-    ];
+function getCharacterClass(scores, allowedSources) {
+    let availableClasses = getOseClasses(allowedSources);
 
+    // Leave only the classes that the character qualifies for.
     let validClasses = availableClasses.filter(characterClass => {
-        if (characterClass.requiredScores.length === 0) {
+        if (characterClass.requirements.length === 0) {
             return true;
         }
 
-        return characterClass.requiredScores.every(score => scores[score] >= 9);
+        return characterClass.requirements.every(score => scores[score] >= 9);
     });
 
-    // For all valid classes, calculate the experience bonus.
+    // Calculate the experience bonus for each valid class.
     validClasses.forEach(characterClass => {
         characterClass.expBonus = getExpBonus(characterClass, scores);
     });
 
+    // Console log the scores.
+    console.log(scores);
     // Console log all valid classes and their experience bonuses.
     validClasses.forEach(characterClass => {
         console.log(`${characterClass.name}: ${characterClass.expBonus}`);
@@ -147,70 +128,8 @@ function getCharacterClass(scores) {
     let maxExpBonus = Math.max(...validClasses.map(characterClass => characterClass.expBonus));
     validClasses = validClasses.filter(characterClass => characterClass.expBonus === maxExpBonus);
 
-    // If there are multiple classes with the same highest experience bonus, pick one at random.
-    let index = Math.floor(Math.random() * validClasses.length);
-    return validClasses[index];
-}
-
-function getExpBonus(classData, scores) {
-    let primeFive = classData.primeFive;
-    let primeTen = classData.primeTen;
-
-    if (!primeTen) {
-        if (scores[primeFive] >= 16) {
-            return 10;
-        } else if (scores[primeFive] >= 13) {
-            return 5;
-        } else {
-            return 0;
-        }
-    } else {
-        if (primeTen.includes("or")) {
-            // Split the primeTen string into an array of words.
-            let primeTenWords = primeTen.split(" ");
-            let score1 = primeTenWords[0];
-            let score2 = primeTenWords[2];
-
-            // If either score is 16 or higher and the other is 13 or higher, return 10.
-            if ((scores[score1] >= 16 && scores[score2] >= 13) || (scores[score1] >= 13 && scores[score2] >= 16)) {
-                return 10;
-            }
-        } else if (primeTen.includes("and")) {
-            // Split the primeTen string into an array of words.
-            let primeTenWords = primeTen.split(" ");
-            let score1 = primeTenWords[0];
-            let score1Min = parseInt(primeTenWords[1]);
-            let score2 = primeTenWords[3];
-            let score2Min = parseInt(primeTenWords[4]);
-
-            if (scores[score1] >= score1Min && scores[score2] >= score2Min) {
-                return 10;
-            }
-        } else {
-            console.log("Invalid primeTen");
-        }
-
-        let primeFiveWords = primeFive.split(" ");
-        let fiveFirst = primeFiveWords[0];
-        let fiveFirstMin = parseInt(primeFiveWords[1]);
-        let fiveOperator = primeFiveWords[2];
-        let fiveSecond = primeFiveWords[3];
-        let fiveSecondMin = parseInt(primeFiveWords[4]);
-
-        if (fiveOperator === "or") {
-            if (scores[fiveFirst] >= fiveFirstMin || scores[fiveSecond] >= fiveSecondMin) {
-                return 5;
-            }
-        } else if (fiveOperator === "and") {
-            if (scores[fiveFirst] >= fiveFirstMin && scores[fiveSecond] >= fiveSecondMin) {
-                return 5;
-            }
-        } else {
-            console.log("Invalid primeFive");
-        }
-
-        return 0;
-    }
+    // If there are multiple classes with the same highest experience bonus, pick one at random.    
+    return validClasses[Math.floor(Math.random() * validClasses.length)];
 }
 
 function generateNames(event) {
